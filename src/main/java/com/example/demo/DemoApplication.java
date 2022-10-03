04da99773ad1aa6e73175d8790df6179e004c91c
yleminc-bhumireddy <108275426+yleminc-bhumireddy@users.noreply.github.com>
2022-10-03 17:49:40 -0400

    changed to getmapping.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RestController
@@ -13,7 +14,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	String sayHello() {
 		return "Hello World!";
 	}
